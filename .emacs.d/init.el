165dcf01b2d24c94c305f648d92218b45ee0df50
mmyoji <mmyoji@gmail.com>
2015-08-22 00:06:28 +0900

    change wakatime path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -118,7 +118,7 @@
  '(js-indent-level 2)
  '(terraform-indent-level 2)
  '(wakatime-api-key "3dc56227-f00c-46e1-a88e-1be22e76714a")
- '(wakatime-cli-path "/usr/local/bin/wakatime"))
+ '(wakatime-cli-path "/Users/mmyoji/.pyenv/shims/wakatime"))
 (require 'wgrep-ag)
 (autoload 'wgrep-ag-setup "wgrep-ag")
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
