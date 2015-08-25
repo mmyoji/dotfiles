8c06416374ab4a9dc3156b2abb0e8e75de4d1b28
mmyoji <mmyoji@gmail.com>
2015-08-25 13:02:14 +0900

    ignore .emacs.d/elpa/*

--- .emacs.d/elpa/flymake-coffee-20140809.324/flymake-coffee-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymake-coffee-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymake-coffee-load) "flymake-coffee" "flymake-coffee.el"
-;;;;;;  (21721 38679 771697 470000))
-;;; Generated autoloads from flymake-coffee.el
-
-(autoload 'flymake-coffee-load "flymake-coffee" "\
-Configure flymake mode to check the current buffer's coffeescript syntax.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flymake-coffee-pkg.el") (21721 38679
-;;;;;;  794771 166000))
-
-;;;***
-
-(provide 'flymake-coffee-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-coffee-autoloads.el ends here
