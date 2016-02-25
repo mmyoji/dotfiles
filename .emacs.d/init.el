379c68a6c42d9ab87185a0b714d61720f5702a8b
mmyoji <mmyoji@gmail.com>
2016-02-25 22:55:58 +0900

    ignore pbcopy area in init.el when OS is not MacOS

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -179,8 +179,9 @@
     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
       (process-send-string proc text)
       (process-send-eof proc))))
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+(when (eq system-type 'darwin)
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
 
 ;; view folders tree structure
 (require 'neotree)
