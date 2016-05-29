8c7e77fdc874eac3d2bf756d4d93b77de739bbc8
mmyoji <mmyoji@gmail.com>
2016-05-29 11:39:01 +0900

    :put_litter_in_its_place: unnecessary json color

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -346,13 +346,6 @@
 (require 'json-mode)
 (add-to-list 'auto-mode-alist '("\\.tss$" . json-mode))
 (add-hook 'json-mode-hook '(lambda() (setq js-indent-level 2)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(erb-face ((t (:background "color-20"))))
- '(fringe ((t (:background "color-20")))))
 
 (provide 'init)
 ;;; init.el ends here
