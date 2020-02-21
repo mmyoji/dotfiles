f5de8d47d489cc341b1a4d89f71e51aa3e23271a
mmyoji <mmyoji@gmail.com>
2020-02-21 21:54:56 +0900

    [WSL] Tweak setup script

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -13,7 +13,7 @@
 #
 # ### Then run this script ###
 # $ curl -sSL -o ./init.sh https://gitlab.com/mmyoji/dotfiles/raw/master/wsl-setup.sh
-# $ bash init.sh
+# $ GIT_VERSION=v2.25.1 bash init.sh
 #
 #
 # ### After running script ###
@@ -37,8 +37,6 @@ sudo apt update -y && sudo apt install -y \
   git \
   htop \
   jq \
-  libmysqlclient-dev \
-  libpq-dev \
   libssl-dev \
   make \
   neovim \
@@ -64,25 +62,16 @@ if [ ! -d ~/.fzf ]; then
 fi
 
 # git-completion #
-git_version=v2.21.0
 if [ ! -e ~/git-completion.bash ]; then
   curl -sSL -o ~/git-completion.bash \
-    https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-completion.bash
+    https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash
 fi
 
 # git-prompt #
 if [ ! -e $HOME/git-prompt.sh ]; then
   curl -sSL -o ~/git-prompt.sh \
-    https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-prompt.sh
+    https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-prompt.sh
 fi
-
-# z #
-z_version=v1.11
-if [ ! -e $HOME/z.sh ]; then
-  curl -sSL -o ~/z.sh \
-    https://raw.githubusercontent.com/rupa/z/$z_version/z.sh
-fi
-
 
 # Apply my custom dotfiles
 [ -e ~/.commit_template ] || ln -s $HOME/src/gitlab.com/mmyoji/dotfiles/.commit_template  ~/
@@ -90,11 +79,5 @@ fi
 [ -e ~/.gitconfig ]       || ln -s $HOME/src/gitlab.com/mmyoji/dotfiles/.gitconfig        ~/
 [ -e ~/.tmux.conf ]       || ln -s $HOME/src/gitlab.com/mmyoji/dotfiles/.tmux.conf        ~/
 
-mkdir -p ~/.config/nvim   || cp $HOME/src/gitlab.com/mmyoji/dotfiles/.vimrc               ~/.config/nvim/init.vim
-
-# Edit after running script:
-#   - Several $HOME to $HOME
-#   - Add $HOME/bin to $PATH
-#   - Set prompt setting to WSL specific
-[ -e ~/.profile ]         || cp $HOME/src/gitlab.com/mmyoji/dotfiles/.profile-brew ~/.profile
+mkdir -p ~/.config/nvim   && cp $HOME/src/gitlab.com/mmyoji/dotfiles/.vimrc               ~/.config/nvim/init.vim
 
